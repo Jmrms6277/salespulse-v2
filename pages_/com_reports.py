1d86cf85c8689a792831a04ccf4879e38d3168d4
Jmrms@6277 <jagadeeshkumar.pulicheri@gmail.com>
2026-04-23 11:16:02 +0530

    latest updates

--- pages_/com_reports.py
+++ pages_/com_reports.py
@@ -1,4 +1,4 @@
 import streamlit as st
 from pages_.coming_soon import coming_soon
 def show():
-    coming_soon("Common Reports", "🔍", "Full Reports With Filters")
+    coming_soon("Reports", "🔍", "Full Reports With Filters")
