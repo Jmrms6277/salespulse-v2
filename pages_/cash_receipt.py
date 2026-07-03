bbf5e8738a10260a5a47435d88caf8a58f0dd1ba
Jmrms@6277 <jagadeeshkumar.pulicheri@gmail.com>
2026-07-03 16:05:49 +0530

    Your commit message here

--- pages_/cash_receipt.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-from pages_.coming_soon import coming_soon
-def show():
-    coming_soon("Cash Receipt", "💵", "Cash Receipt will show daily cash collection, receipts and payment tracking.")
